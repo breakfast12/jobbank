d7f81514424c81d8654598e50727e087bb3c5c42
breakfast12 <fresaridhom@gmail.com>
2025-02-06 23:01:39 +0700

    Integration front end homepage and master layout blade

---
+++ resources/views/front/details.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/output.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
+    <!-- CSS for carousel/flickity-->
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+</head>
+
+<body class="font-poppins text-[#0E0140] pb-[100px] overflow-x-hidden">
+    <div id="page-background" class="absolute h-[533px] w-full top-0 -z-10 overflow-hidden">
+        <img src="{{ asset('assets/backgrounds/Group 2009.png') }}" class="w-full h-full object-fill" alt="background">
+    </div>
+</body>
