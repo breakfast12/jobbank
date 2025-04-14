c363cc6971560d86c663d6b0539d2ab3abb9fc89
breakfast12 <fresaridhom@gmail.com>
2025-04-14 20:20:58 +0700

    reuseable nav

--- resources/views/front/apply.blade.php
+++ resources/views/front/apply.blade.php
@@ -5,44 +5,7 @@
         <div id="page-background" class="absolute h-[533px] w-full top-0 -z-10 overflow-hidden">
             <img src="{{ asset('assets/backgrounds/Group 2009.png') }}" class="w-full h-full object-fill" alt="background">
         </div>
-        <nav class="container max-w-[1130px] mx-auto flex items-center justify-between pt-10">
-            <a href="index.html" class="flex shrink-0">
-                <img src="{{ asset('assets/logos/Logo.svg') }}" alt="Logo">
-            </a>
-            <ul class="flex items-center gap-10">
-                <li>
-                    <a href="index.html" class="transition-all duration-300 hover:font-semibold hover:text-[#FF6B2C] font-semibold text-[#FF6B2C]">Home</a>
-                </li>
-                <li>
-                    <a href="index.html" class="transition-all duration-300 hover:font-semibold hover:text-[#FF6B2C] font-medium text-white">Features</a>
-                </li>
-                <li>
-                    <a href="index.html" class="transition-all duration-300 hover:font-semibold hover:text-[#FF6B2C] font-medium text-white">Benefits</a>
-                </li>
-                <li>
-                    <a href="index.html" class="transition-all duration-300 hover:font-semibold hover:text-[#FF6B2C] font-medium text-white">Stories</a>
-                </li>
-                <li>
-                    <a href="index.html" class="transition-all duration-300 hover:font-semibold hover:text-[#FF6B2C] font-medium text-white">About</a>
-                </li>
-            </ul>
-            @guest
-                <div class="flex items-center gap-3">
-                    <a href="{{ route('login') }}" class="rounded-full border border-white p-[14px_24px] font-semibold text-white">Sign In</a>
-                    <a href="{{ route('register') }}" class="rounded-full p-[14px_24px] bg-[#FF6B2C] font-semibold text-white hover:shadow-[0_10px_20px_0_#FF6B2C66] transition-all duration-300">Sign up</a>
-                </div>
-            @endguest
-            @auth
-                <div class="flex items-center gap-4">
-                    <p class="username font-medium text-white">Hi, {{ Auth::user()->name }}</p>
-                    <div class="w-[52px] h-[52px] flex shrink-0 rounded-full overflow-hidden">
-                        <a href="{{ route('dashboard') }}">
-                            <img src="{{ Storage::url(Auth::user()->avatar) }}" class="object-cover w-full h-full" alt="photo">
-                        </a>
-                    </div>
-                </div>
-            @endauth
-        </nav>
+        <x-nav />
         <form action="{{ route('front.apply.store', $companyJob->slug) }}" method="POST" enctype="multipart/form-data" class="relative max-w-[900px] mx-auto flex flex-col rounded-[20px] bg-white border border-[#E8E4F8] p-[30px] gap-10 shadow-[0_8px_30px_0_#0E01400D] mt-[130px]">
             @csrf
             <div class="absolute -top-[60px] left-[50px] w-[120px] h-[120px] p-5 flex shrink-0 items-center justify-center bg-white rounded-[20px] border border-[#E8E4F8] shadow-[0_8px_30px_0_#0E01400D]">
